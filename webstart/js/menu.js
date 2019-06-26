6f3a0fd4b0e37edaba4bee46b266815d08d2cacf
aleg010196 <48119524+aleg010196@users.noreply.github.com>
2019-06-26 20:24:31 +0500

    диплом

---
+++ webstart/js/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var menu = document.querySelector('#item')
+    var burger = document.querySelector('#menu');
+    burger.addEventListener('click', function () {
+        menu.classList.add('navbar__item_active')
+    });
+    burger.addEventListener('click', function () {
+        menu.classList.add('navbar__item_active')
+    });
+
+});
